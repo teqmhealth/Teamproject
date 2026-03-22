c55fb45704eb62a594144722964032a8f7f33df2
teqmhealth <g741741ood@gmail.com>
2026-03-22 23:49:21 +0300

    Update ecg_model.py

--- ecg_model.py
+++ ecg_model.py
@@ -1,8 +1,8 @@
 import os, requests, numpy as np
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.utils import to_categorical
+from keras import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,18 +17,19 @@ def train_ecg_model():
     readings = supabase_request("tbl_ecg?select=*")
     X, y = [], []
     for r in readings:
-        if r.get("signal_value") is not None and r.get("diagnosis_label") is not None:
+        if r.get("signal_value") and r.get("diagnosis_label") is not None:
             X.append([r["signal_value"]])
             y.append(r["diagnosis_label"])
-    if not X:
-        return {"error": "لا توجد بيانات مكتملة"}
+    if not X: return {"error": "لا توجد بيانات مكتملة"}
     X, y = np.array(X), np.array(y)
     y_cat = to_categorical(y, num_classes=2)
     X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=0.3)
-    model = Sequential([Dense(16, activation='relu', input_shape=(1,)),
-                        Dense(8, activation='relu'),
-                        Dense(2, activation='softmax')])
+    model = Sequential([
+        Dense(16, activation='relu', input_shape=(1,)),
+        Dense(8, activation='relu'),
+        Dense(2, activation='softmax')
+    ])
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(X_train, y_train, epochs=50, batch_size=8, verbose=0)
-    model.save("ecg_model.h5")
+    model.save("ecg_model.keras")
     return {"accuracy": float(model.evaluate(X_test, y_test, verbose=0)[1])}
