deb55daf8b306669262b828a4fd6bbe781705acc
teqmhealth <g741741ood@gmail.com>
2026-03-22 06:03:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,33 @@ def get_all_patients():
     }
     response = requests.get(url, headers=headers)
     return response.json()
+def get_all_reports():
+    url = f"{SUPABASE_URL}/rest/v1/tbl_report?select=*"
+    headers = {
+        "apikey": SUPABASE_KEY,
+        "Authorization": f"Bearer {SUPABASE_KEY}"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+def get_all_reading():
+    url = f"{SUPABASE_URL}/rest/v1/tbl_reading?select=*"
+    headers = {
+        "apikey": SUPABASE_KEY,
+        "Authorization": f"Bearer {SUPABASE_KEY}"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
 
 # Endpoint يعيد كل المرضى
 @app.get("/patients")
 def read_patients():
     patients = get_all_patients()
     return {"patients": patients}
+@app.get("/reports")
+def read_reports():
+    reports = get_all_reports()
+    return {"reports": reports}
+    @app.get("/reading")
+def read_readers():
+    reports = get_all_reading()
+    return {"readers": reports}
